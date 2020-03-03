308801e10747002614982bf3f23d762dfc557536
Ryan-ops <57469976+Ryan-ops@users.noreply.github.com>
2020-03-02 20:37:10 -0500

    added color folder

---
+++ color/js/auth.js
@@ -0,0 +1,29 @@
+const loginEmail = document.getElementById("login-email");
+const loginPassword = js.getEl("login-password");
+const loginButton = js.getEl("login-button");
+const loginMessage = js.getEl("login-message");
+const userName = js.getEl("user-name");
+
+/*
+	event listener 
+	listening user input, mouse clicks or keyboard presses
+	onclick captures a click event
+*/
+
+loginButton.onclick = function() {
+	fb.login(loginEmail.value, loginPassword.value);
+};
+//adds login for hitting 
+loginPassword.addEventListener('keyup',function () {
+    if (event.which == 13){
+        fb.login(loginEmail.value, loginPassword.value);
+    }
+});
+
+function onError(errorMessage) {
+	loginMessage.textContent = errorMessage;
+}
+
+function userLoggedIn(uid, displayName) {
+	userName.textContent = "Welcome " + displayName + ".";
+}
